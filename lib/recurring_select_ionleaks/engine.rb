08bfb44110df6c42a29cdfc707c7b7429b8f2670
Rene Vahtel <rene.vahtel@gmail.com>
2025-05-02 12:05:24 +0700

    Pre-require helper and middleware files to improve initialization process

--- lib/recurring_select_ionleaks/engine.rb
+++ lib/recurring_select_ionleaks/engine.rb
@@ -1,15 +1,19 @@
+# Explicitly require the files to prevent autoloading during initialization
+require_relative '../../app/helpers/recurring_select_helper'
+require_relative '../../app/middleware/recurring_select_middleware'
+
 module RecurringSelectIonleaks 
   class Engine < Rails::Engine
-    
+    # Initializer for extending FormBuilder, Helpers are now pre-required
     initializer "recurring_select_ionleaks.extending_form_builder" do |app|
-    # config.to_prepare do
       ActionView::Helpers::FormHelper.send(:include, RecurringSelectHelper::FormHelper)
       ActionView::Helpers::FormOptionsHelper.send(:include, RecurringSelectHelper::FormOptionsHelper)
       ActionView::Helpers::FormBuilder.send(:include, RecurringSelectHelper::FormBuilder)
     end
     
+    # Initializer for connecting middleware, Middleware class is now pre-required
     initializer "recurring_select_ionleaks.connecting_middleware" do |app|
-      app.middleware.use RecurringSelectMiddleware # insert_after ActionDispatch::ParamsParser, 
+      app.middleware.use RecurringSelectMiddleware
     end
     
   end
